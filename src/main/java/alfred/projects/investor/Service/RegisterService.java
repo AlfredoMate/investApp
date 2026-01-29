e4c1c2ea3810cea718b3d90065a65e53a352d411
Alfredo <mate.alfredo98@gmail.com>
2026-01-29 20:46:24 +0100

    feature : added H2 in memory database

--- src/main/java/alfred/projects/investor/Service/RegisterService.java
+++ src/main/java/alfred/projects/investor/Service/RegisterService.java
@@ -1,21 +1,26 @@
 package alfred.projects.investor.Service;
 
+import alfred.projects.investor.Persistance.User;
 import alfred.projects.investor.Persistance.UserPersistance;
+import alfred.projects.investor.Persistance.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
 
-    private UserPersistance userPersistance;
+    private UserRepository userRepository;
 
-    public RegisterService (UserPersistance userPersistance) {
-        this.userPersistance = userPersistance;
+    public RegisterService (UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+
     }
 
     public void registerLogic (String username, String password) {
 
         String hashedPassword = HashingService.hashPassword(password);
-        userPersistance.addUser(username, hashedPassword);
+        User user = new User(username, hashedPassword);
+        userRepository.storeUser(user);
 
     }
 }
