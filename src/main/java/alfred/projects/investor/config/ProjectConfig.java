72212c44efb8222208176d69ac098285ea6eac2e
Alfredo <mate.alfredo98@gmail.com>
2026-01-30 19:44:44 +0100

    feat : changed JDBC driver to mySQL

--- src/main/java/alfred/projects/investor/config/ProjectConfig.java
+++ src/main/java/alfred/projects/investor/config/ProjectConfig.java
@@ -1,15 +1,29 @@
 package alfred.projects.investor.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableFeignClients (
         basePackages = "alfred.projects"
 )
 public class ProjectConfig implements WebMvcConfigurer {
+
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -19,6 +33,15 @@ public class ProjectConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .exposedHeaders("Username");
+    }
+
+    @Bean
+    public DataSource dataSource () {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl(jdbcUrl);
+        hikariDataSource.setUsername(username);
+        hikariDataSource.setPassword(password);
+        return hikariDataSource;
     }
 
 }
