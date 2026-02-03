3c66d9ea8c17a4c61abeebad9bd2b3e0e6a145ca
Alfredo <mate.alfredo98@gmail.com>
2026-02-03 11:09:03 +0100

    feat (Controllers): unit tests for RegisterController and LoginController

---
+++ src/test/java/alfred/projects/investor/Controller/RegisterControllerTest.java
@@ -0,0 +1,32 @@
+package alfred.projects.investor.Controller;
+
+import alfred.projects.investor.Controllers.RegisterController;
+import alfred.projects.investor.Service.RegisterService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class RegisterControllerTest {
+
+    @Mock
+    private RegisterService registerService;
+
+    @InjectMocks
+    private RegisterController registerController;
+
+    private final String USERNAME = "Joe";
+    private final String PASSWORD = "password";
+
+    @Test
+    @DisplayName("Happy path flow unit test of register method")
+    public void registerHappyPathFlow() {
+        registerController.register(USERNAME, PASSWORD);
+        verify(registerService).registerLogic(USERNAME, PASSWORD);
+    }
+}
