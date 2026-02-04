3fa79b517c94d191c36f5a3f82aec288dab6a926
Alfredo <mate.alfredo98@gmail.com>
2026-02-04 20:49:41 +0100

    test: add integration tests for not happy paths for Login and Register

--- src/test/java/alfred/projects/investor/integration/LoginControllerIT.java
+++ src/test/java/alfred/projects/investor/integration/LoginControllerIT.java
@@ -5,6 +5,7 @@ import alfred.projects.investor.Persistance.User;
 import alfred.projects.investor.Persistance.UserRepository;
 import alfred.projects.investor.Service.HashingService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,23 +29,43 @@ public class LoginControllerIT {
     @Autowired
     private UserRepository userRepository;
 
-    private final String USERNAME = "Joe";
+    private final String EXISTING_USERNAME = "Joe";
+    private final String UNEXISTING_USERNAME = "John";
     private final String PASSWORD = "password";
+    private final String INCORRECT_PASSWORD = "incorrect_password";
 
     @BeforeEach
     public void setUp () {
         userRepository.deleteAll();
         String password_hashed = hashingService.hashPassword(PASSWORD);
-        User user = new User(USERNAME, password_hashed);
+        User user = new User(EXISTING_USERNAME, password_hashed);
         userRepository.save(user);
     }
 
+    @DisplayName("Happy path integration test for validateLogin method")
     @Test
-    public void testValidateLogin () throws Exception {
+    public void testValidateLoginHappyPath () throws Exception {
         mockMvc.perform(post("/login")
-                .header("Username", USERNAME)
+                .header("Username", EXISTING_USERNAME)
                 .header("Password", PASSWORD))
                 .andExpect(status().isOk());
     }
 
+    @DisplayName("Integration test for ValidateLogin mehtod when user doesn't exist.")
+    @Test
+    public void testValidateLoginUserDoesntExist () throws Exception {
+        mockMvc.perform(post("/login")
+                .header("Username", UNEXISTING_USERNAME)
+                .header("Password", PASSWORD))
+                .andExpect(status().is(404));
+    }
+
+    @DisplayName("Integration test for ValidateLogin mehtod when password is incorrect.")
+    @Test
+    public void testValidateLoginUserIncorrectPssword () throws Exception {
+        mockMvc.perform(post("/login")
+                .header("Username", EXISTING_USERNAME)
+                .header("Password", INCORRECT_PASSWORD))
+                .andExpect(status().is(401));
+    }
 }
--- src/test/java/alfred/projects/investor/integration/RegisterControllerIT.java
+++ src/test/java/alfred/projects/investor/integration/RegisterControllerIT.java
@@ -3,6 +3,7 @@ package alfred.projects.investor.integration;
 import alfred.projects.investor.Persistance.User;
 import alfred.projects.investor.Persistance.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +24,31 @@ public class RegisterControllerIT {
     private UserRepository userRepository;
 
     private final String USERNAME = "Joe";
+    private final String ALREADY_EXISTING_USERNAME = "John";
     private final String PASSWORD = "password";
 
     @BeforeEach
     void setUp() {
         userRepository.deleteAll();
+        User alreadyExistingUser = new User(ALREADY_EXISTING_USERNAME, PASSWORD);
+        userRepository.save(alreadyExistingUser);
     }
+
+    @DisplayName("Happy path integration test for register method")
     @Test
-    void testGetUserById() throws Exception {
+    void testRegister() throws Exception {
         mockMvc.perform(post("/register")
                         .header("username", USERNAME)
                         .header("password", PASSWORD)
                 ).andExpect(status().isOk());
+    }
+
+    @DisplayName("Integration test for register method when user already exists.")
+    @Test
+    void testRegisterUserAlreadyExists() throws Exception {
+        mockMvc.perform(post("/register")
+                .header("username", ALREADY_EXISTING_USERNAME)
+                .header("password", PASSWORD))
+                .andExpect(status().is(409));
     }
 }
